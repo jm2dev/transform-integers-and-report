7bb3b4f98da746e606ceb56c0904f40b99965b26
Jose Miguel <jm@0pt1mates.com>
2016-03-21 17:33:41 +0000

    STEP 1: transform input number depending on its divisors.

---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,28 @@
+package com.jm2dev.ee
+
+import scala.collection.immutable.Range.Inclusive
+
+object FizzBuzz {
+  private val divisors = List(
+    (3, "fizz"),
+    (5, "buzz"))
+
+  def transform(input: Inclusive): String = input.map(transform(_)).mkString(" ")
+
+  def transform(number: Int): String = {
+    val results = divisors.map((divisor) => divisibleBy(number, divisor._1, divisor._2))
+      .filter(_.nonEmpty)
+
+    results.length match {
+      case 0 => s"$number"
+      case _ => results.map(_.getOrElse("")).mkString("")
+    }
+  }
+
+  private def divisibleBy(number: Int, divisor: Int, result: String): Option[String] = {
+    number % divisor match {
+      case 0 => Some(result)
+      case _ => None
+    }
+  }
+}
---
+++ src/test/scala/FizzBuzzSpec.scala
@@ -0,0 +1,37 @@
+package com.jm2dev.ee
+
+import org.specs2.Specification
+
+import scala.collection.immutable.Range.Inclusive
+
+class FizzBuzzSpec extends Specification {
+  def is =
+    s2"""
+        Given an integer number
+          when is multiple of 3 then 'fizz' is returned                     $e1
+          when is multiple of 5 then 'buzz' is returned                     $e2
+          when is multiple of 15 then 'fizzbuzz' is returned                $e3
+          when anything else then input number is returned                  $e4
+          when is 0 then 'fizzbuzz' is returned                             $e5
+
+        Given a list of integer numbers as input
+          when range 1 to 20 then expected output is returned               $e6
+          when range has negative numbers then expected output is returned  $e7
+      """
+
+  def e1 = FizzBuzz.transform(6) must be equalTo "fizz"
+  def e2 = FizzBuzz.transform(5) must be equalTo "buzz"
+  def e3 = FizzBuzz.transform(15) must be equalTo "fizzbuzz"
+  def e4 = FizzBuzz.transform(2) must be equalTo "2"
+  def e5 = FizzBuzz.transform(0) must be equalTo "fizzbuzz" //(all non-zero integers are divisors of 0)
+
+  val input = 1 to 20
+  val expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz"
+
+  def e6 = FizzBuzz.transform(input) must be equalTo expected
+
+  val inputWithNegativeIntegers = -5 to 5
+  val expectedForNegativeIntegers = "buzz -4 fizz -2 -1 fizzbuzz 1 2 fizz 4 buzz"
+
+  def e7 = FizzBuzz.transform(inputWithNegativeIntegers) must be equalTo expectedForNegativeIntegers
+}
